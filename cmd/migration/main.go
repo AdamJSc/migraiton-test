eddd14f7c09a66ff7c24e3cf5531f5ef3e2bc28c
Adam Scholey <hello@adamjs.net>
2021-01-15 17:35:22 +0000

    Setup source and database driver instances + migration instance

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mongodb"
+	"github.com/golang-migrate/migrate/v4/source/file"
+	"go.mongodb.org/mongo-driver/mongo"
+	_ "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"time"
 )
 
 func main() {
+	// cli bootstrap
 	var up, down bool
 	flag.BoolVar(&up, "up", false, "bring migrations up")
 	flag.BoolVar(&down, "down", false, "bring migrations down")
@@ -14,6 +23,53 @@ func main() {
 	if (up && down) || (!up && !down) {
 		log.Fatal("please set either -up or -down flags")
 	}
+
+	// setup
+	var (
+		ctx = context.Background()
+
+		sourceName = "local data files"
+		sourcePath = "file://data" // migrations file source directory
+
+		conn   = "mongodb://testUser:testPass@localhost:27017" // mongo connection string
+		connTO = 5                                             // mongo connection timeout in secs
+		dbName = "myDB"                                        // mongo db name
+
+	)
+
+	// mongo client
+	mc, err := mongo.NewClient(
+		options.Client().ApplyURI(conn),
+		options.Client().SetConnectTimeout(time.Duration(connTO)*time.Second),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := mc.Connect(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	// mongo migration driver
+	cnf := &mongodb.Config{DatabaseName: dbName}
+	md, err := mongodb.WithInstance(mc, cnf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// source driver
+	f := &file.File{}
+	source, err := f.Open(sourcePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// build migration client
+	mig, err := migrate.NewWithInstance(sourceName, source, dbName, md)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(mig)
 
 	switch {
 	case up:
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module migrationtest
 
 go 1.15
 
-require github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+require (
+	github.com/aws/aws-sdk-go v1.36.27 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.14.1 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
+	go.mongodb.org/mongo-driver v1.4.4 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
